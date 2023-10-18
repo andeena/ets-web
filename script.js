452a6d64984624360035c7e2761421b6ccf9e836
andeena <113409268+andeena@users.noreply.github.com>
2023-10-18 09:55:25 +0700

    ets

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-// const element = document.getElementById("title");
-// const plot = document.getElementById("plot");
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://it-its.id/api/movies',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      displayMovies(data);
+    },
+    error: function(err) {
+      console.log('Error:', err);
+    }
+  });
+});
 
-// document.getElementById("title").innerHTML = element.innerHTML;
-// document.getElementById("plot").innerHTML = element.innerHTML;
-function addMovie() {
-    const apiUrl = `https://it-its.id/api/movies?query=${searchTerm}`;
+function displayMovies(data) {
+  var movieTable = $('#movieTable');
+  var row = $('<div class="row"></div>');
 
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'it-its.id/api/movies',
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const movieList = document.getElementById('movie-list');
+  data.forEach(function(movie, index) {
+    var card = `
+      <div class="col-md-6">
+        <div class="card my-3">
+          <div class="card-body">
+            <h5 class="card-title">${movie.Title}</h5>
+            <p class="card-text">${movie.Plot}</p>
+          </div>
+        </div>
+      </div>
+    `;
+    row.append(card);
 
-        data.titles.forEach(movie => {
-            const movieCard = document.createElement('div');
-            movieCard.classList.add('col-md-6', 'mb-4');
-
-            const movieTitle = document.createElement('h5');
-            movieTitle.textContent = movie.title;
-            movieCard.appendChild(movieTitle);
-
-            const moviePlot = document.createElement('p');
-            moviePlot.textContent = movie.plot;
-            movieCard.appendChild(moviePlot);
-
-            movieList.appendChild(movieCard);
-        });
-    })
-    .catch(error => console.error('Error fetching data:', error));
+    // Add a new row after every 2 cards
+    if ((index + 1) % 2 === 0 || index === data.length - 1) {
+      movieTable.append(row);
+      row = $('<div class="row"></div>');
+    }
+  });
 }
-
