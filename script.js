b23f51088aeaa0fd6baff0ad5f9fcb01f8410205
andeena <113409268+andeena@users.noreply.github.com>
2023-10-25 08:13:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
-  $.ajax({
-    url: 'https://it-its.id/api/movies',
-    method: 'GET', // ga terlalu ngaruh
-    dataType: 'json',
-    success: function(data) {
-      displayMovies(data);
-    },
-    error: function(err) {
-      console.log('Error:', err);
-    }
+
+  //manggil link api pake ajax trs pake method get
+    $.ajax({
+      url: 'https://it-its.id/api/movies',
+      // method: 'GET',
+      // dataType: 'json', //respon
+      success: function(data) { //manggil func buat display filmnya
+        displayMovies(data);
+      },
+      error: function(err) {
+        console.log('Error:', err);
+      }
+    });
   });
-});
-
-function displayMovies(data) {
-  var movieTable = $('#movieTable');
-  var row = $('<div class="row"></div>');
-
-  data.forEach(function(movie, index) {
-    var card = `
-      <div class="col-md-6">
-        <div class="card my-3">
-          <div class="card-body">
-            <h5 class="card-title">${movie.Title}</h5>
-            <p class="card-text">${movie.Plot}</p>
+  
+  //ini func buat nampilin film
+  function displayMovies(data) {
+    var movieTable = $('#movieTable');
+    var row = $('<div class="row"></div>');
+  
+    //bikin card
+    data.forEach(function(movie, index) {
+      var card = `
+        <div class="col-md-6">
+          <div class="card my-3">
+            <div class="card-body">
+              <h5 class="card-title">${movie.Title}</h5>
+              <p class="card-text">${movie.Plot}</p>
+            </div>
           </div>
         </div>
-      </div>
-    `;
-    row.append(card);
-
-    if ((index + 1) % 2 === 0 || index === data.length - 1) {
-      movieTable.append(row);
-      row = $('<div class="row"></div>');
-    }
-  });
-}
+      `;
+      row.append(card);
+  
+      //nambahin card tiap udah akhir data
+      if ((index + 1) % 2 === 0 || index === data.length - 1) {
+        movieTable.append(row);
+        row = $('<div class="row"></div>');
+      }
+    });
+  }
+  
